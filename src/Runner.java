2203032414d2f595dc6fafb541ed4fadc9cd92ad
Serkan <ozsahinserkan@gmail.com>
2023-09-12 11:17:06 +0300

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Veritabani.baslangicOgretmenVeritabaniOlustur();
+        Veritabani.baslangicOgrenciVeritabaniOlustur();
+        Helper.anaMenu();
+    }
+}
